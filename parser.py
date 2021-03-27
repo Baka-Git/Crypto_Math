1ff6ce6be248d83d49e22a5d2b6c53c6331c5900
Baka-Git <80925229+Baka-Git@users.noreply.github.com>
2021-03-27 23:32:11 +0100

    Add Some Basic Function
    add GCD,  Factorized

--- parser.py
+++ parser.py
@@ -1,14 +1,28 @@
 import argparse
 
+
 def parse():
-    print("  @@@   @@@@  @@@@@@@@\n @@ @@  @@ @@    @@\n@@      @@ @@    @@\n@@      @@@@     @@   Tools for Cryptography\n @@ @@  @@       @@   Mady by: Baka\n  @@@   @@       @@")
+    print(
+        "  @@@   @@@@  @@@@@@@@\n @@ @@  @@ @@    @@\n@@      @@ @@    @@\n@@      @@@@     @@   Tools for Cryptography\n @@ @@  @@       @@   Mady by: Baka\n  @@@   @@       @@")
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--detection",
-                        help="Enables Detection Module. Detection to enable: syn - Syn Flood detection, udp - UDP "
-                             "Flood detection, icmp - ICMP Flood detection, complex - Complex detection. Example '-d "
-                             "syn', '-d syn,udp' In case of enabling all detetion modes use: '-d all'")
+    parser.add_argument("-g", "--gcd", help="'-g 5,6'")
+    parser.add_argument("-f", "--factorize", help="'-f 5'")
+    parser.add_argument("-c", "--crt", help="'-c '")
 
     args = parser.parse_args()
     print(args)
     return args
+
+
+def get_ints(args):
+    list_of_ints = args.split(",")
+    for inter in list_of_ints:
+        try:
+            inter = int(inter)
+        except:
+            print("Arguments are not integer!")
+            return False
+    return list_of_ints
+
+
 parse()
